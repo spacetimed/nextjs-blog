ae80e18db0fe1dbea57dfbb6383784d844139a37
FFFFFF-base16 <s127813127@gmail.com>
2022-06-04 07:17:38 -0700

    re-organized CSS / components / pages

--- blog-app/components/layout.js
+++ blog-app/components/layout.js
@@ -2,12 +2,15 @@ import styles from '../styles/layout.module.css';
 import Navigation from './navigation';
 import Banner from '../components/banner';
 import Footer from '../components/footer';
+import { useRouter } from 'next/router';
 
 export default function Layout({ children }) {
+    const router = useRouter();
+    const isHome = (router.asPath == '/');
     return(
         <div className={styles.container}>
             <Navigation />
-            <Banner />
+            {isHome && <Banner />}
             <div className={styles.content}>
                 {children}
             </div>
